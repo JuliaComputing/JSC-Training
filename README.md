9b3f40b7381b1214cbff2838d6a1dd7c276af2c5
Matt Bauman <mbauman@gmail.com>
2019-09-25 08:49:25 -0500

    Initial import

---
+++ README.md
@@ -0,0 +1,12 @@
+[![Build Status](https://dev.azure.com/JuliaComputing/Julia%20Academy/_apis/build/status/JuliaComputing.JuliaAcademyMaterials)](https://dev.azure.com/JuliaComputing/Julia%20Academy/_build/latest?definitionId=1)
+
+# Source files for Julia Academy Notebooks
+
+- Running `julia --project build.jl` will
+    1. Create a `Notebooks/` directory
+    2. Course content in `Courses/` will get converted to notebook (if `.jl` file) or get directly copied.
+    3. A specific course can be built by giving it as an argument, e.g. `julia --project build.jl SomeCourse`.
+
+# Tips
+
+- If you're using Plots + GR, add `ENV["GKSwstype"] = "100" #src` to your .jl file.
---
+++ build.jl
@@ -0,0 +1,53 @@
+import Pkg
+
+srcpath = joinpath(@__DIR__(), "Courses")
+buildpath = joinpath(@__DIR__(), "Notebooks")
+
+if isempty(ARGS)
+    courses = filter(x->isdir(joinpath(srcpath), x) && x !== "Template", readdir(srcpath))
+else
+    courses = ARGS
+end
+
+rm(buildpath, recursive=true, force=true)
+mkdir(buildpath)
+
+notebooks_failed = String[]
+
+Base.julia_cmd()
+for dir in courses
+    @info "building $dir..."
+    src_course = joinpath(srcpath, dir)
+    build_course = mkdir(joinpath(buildpath, dir))
+    cp(src_course, build_course; force=true)
+
+    Pkg.activate(build_course)
+    test = false
+    Pkg.instantiate()
+    for file in readdir(build_course)
+        if endswith(file, ".jl")
+            course = joinpath(build_course, file)
+            academy_environment = @__DIR__
+            # Create a stacked environment with the JuliaAcademy environment in
+            # it so we can load Literate for each notebook
+            script = """
+            pushfirst!(LOAD_PATH, $(repr(academy_environment)));
+            import Literate;
+            Literate.notebook($(repr(course)), $(repr(build_course)); credit=false, execute=false)
+            """
+            try
+                run(`$(Base.julia_cmd()) -e $script`)
+            catch
+                err_src_course = relpath(joinpath(src_course, file), @__DIR__)
+                @error "Script failed to build: $err_src_course)"
+                push!(notebooks_failed, err_src_course)
+            end
+            rm(course)
+        end
+    end
+end
+
+if !isempty(notebooks_failed)
+    error("The following notebooks failed: \n",
+          join(notebooks_failed, '\n'))
+end
