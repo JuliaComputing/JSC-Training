14a2f52a1b68ca7d294c8c4c4ea427a1f643c018
Matt Bauman <mbauman@gmail.com>
2019-09-25 20:51:29 -0500

    Update Multiple dispatch

--- Courses/Introduction to Julia/10.Multiple-dispatch.jl
+++ Courses/Introduction to Julia/10.Multiple-dispatch.jl
@@ -1,4 +1,4 @@
-# # Multiple dispatch
+# # Multiple Dispatch
 
 #-
 
@@ -12,58 +12,57 @@
 #
 # We can declare functions in Julia without giving Julia any information about the types of the input arguments that function will receive:
 
-f(x) = x^2
-
-# and then Julia will determine on its own which input argument types make sense and which do not:
-
-f(10)
+square(x) = x^2
 
 #-
 
-f([1, 2, 3])
+square(10)
+
+#-
+
+square("Hello ")
+
+#-
+
+square([1,2,3])
 
 # #### Specifying the types of our input arguments
 #
 # However, we also have the *option* to tell Julia explicitly what types our input arguments are allowed to have.
 #
-# For example, let's write a function `foo` that only takes strings as inputs.
+# For example, let's write a function `f` that only takes `Number`s as inputs.
 
-foo(x::String, y::String) = println("My inputs x and y are both strings!")
-
-# We see here that in order to restrict the type of `x` and `y` to `String`s, we just follow the input argument name by a double colon and the keyword `String`.
-#
-# Now we'll see that `foo` works on `String`s and doesn't work on other input argument types.
-
-foo("hello", "hi!")
+f(a::Integer, b::Integer) = "a and b are both integers"
 
 #-
 
-foo(3, 4)
-
-# To get `foo` to work on integer (`Int`) inputs, let's tack `::Int` onto our input arguments when we declare `foo`.
-
-foo(x::Int, y::Int) = println("My inputs x and y are both integers!")
+f(3, 4)
 
 #-
 
-foo(3, 4)
+f(1.2, 3.4)
 
-# Now `foo` works on integers! But look, `foo` also still works when `x` and `y` are strings!
+# But we can define that method!
 
-foo("hello", "hi!")
+f(a::Float64, b::Float64) = "a and b are both Float64s"
+
+#-
+
+f(1.2, 3.4)
 
 # This is starting to get to the heart of multiple dispatch. When we declared
 #
 # ```julia
-# foo(x::Int, y::Int) = println("My inputs x and y are both integers!")
+# f(a::Float64, b::Float64) = "a and b are both Float64s"
 # ```
 # we didn't overwrite or replace
 # ```julia
-# foo(y::String, y::String)```
+# f(a::Integer, b::Integer)
+# ```
 #
-# Instead, we just added an additional ***method*** to the ***generic function*** called `foo`.
+# Instead, we just added an additional ***method*** to the ***function*** called `foo`.
 #
-# A ***generic function*** is the abstract concept associated with a particular operation.
+# A ***function*** is the abstract concept associated with a particular operation.
 #
 # For example, the generic function `+` represents the concept of addition.
 #
@@ -71,58 +70,234 @@ foo("hello", "hi!")
 #
 # For example, `+` has methods that accept floating point numbers, integers, matrices, etc.
 #
-# We can use the `methods` to see how many methods there are for `foo`.
+# We can use the `methods` to see how many methods there are for `f`.
 
-methods(foo)
+methods(f)
 
 # Aside: how many methods do you think there are for addition?
 
 methods(+)
 
-# So, we now can call `foo` on integers or strings. When you call `foo` on a particular set of arguments, Julia will infer the types of the inputs and dispatch the appropriate method. *This* is multiple dispatch.
-#
-# Multiple dispatch makes our code generic and fast. Our code can be generic and flexible because we can write code in terms of abstract operations such as addition and multiplication, rather than in terms of specific implementations. At the same time, our code runs quickly because Julia is able to call efficient methods for the relevant types.
-#
-# To see which method is being dispatched when we call a generic function, we can use the @which macro:
+# ### Basic dispatch
 
-@which foo(3, 4)
-
-# Let's see what happens when we use `@which` with the addition operator!
-
-@which 3.0 + 3.0
-
-# And we can continue to add other methods to our generic function `foo`. Let's add one that takes the ***abstract type*** `Number`, which includes subtypes such as `Int`, `Float64`, and other objects you would think of as numbers:
-
-foo(x::Number, y::Number) = println("My inputs x and y are both numbers!")
-
-# This method for `foo` will work on, for example, floating point numbers:
-
-foo(3.0, 4.0)
-
-# We can also add a fallback, duck-typed method for `foo` that takes inputs of any type:
-
-foo(x, y) = println("I accept inputs of any type!")
-
-# Given the methods we've already written for `foo` so far, this method will be called whenever we pass non-numbers to `foo`:
-
-v = rand(3)
-foo(v, v)
-
-# ### Exercises
-#
-# #### 9.1
-#
-# Extend the function `foo`, adding a method that takes only one input argument, which is of type `Bool`, and prints "foo with one boolean!"
+f(a, b) = "fallback"
+f(a::Number, b::Number) = "a and b are both numbers"
+f(a::Number, b) = "a is a number"
+f(a, b::Number) = "b is a number"
+f(a::Integer, b::Integer) = "a and b are both integers"
 
 #-
 
-# #### 9.2
-#
-# Check that the method being dispatched when you execute
-# ```julia
-# foo(true)
+methods(f)
+
+#-
+
+f(1.5, 2)
+
+#-
+
+f(1, "bar")
+
+#-
+
+f(1, 2)
+
+#-
+
+f("foo", [1,2])
+
+#-
+
+f(1, 2, 3)
+
+# ### Ambiguities
+
+g(a::Int, b::Number) = 1
+g(a::Number, b::Int) = 2
+
+#-
+
+g(1, 2.5)
+
+#-
+
+g(1.5, 2)
+
+#-
+
+g(1, 2)
+
+#-
+
+g(x::Int, y::Int) = 3
+
+#-
+
+g(1, 2)
+
+# ### "Diagonal" dispatch
+
+f(a::T, b::T) where {T<:Number} = "a and b are both $(T)s"
+
+#-
+
+methods(f)
+
+#-
+
+f(big(1.5), big(2.5))
+
+#-
+
+f(big(1), big(2)) # <== integer rule is more specific
+
+#-
+
+f(a::T, b::T) where {T<:Integer} = "both are $T integers"
+
+#-
+
+methods(f)
+
+#-
+
+f(big(1), big(2))
+
+#-
+
+f("foo", "bar") # <== still doesn't apply to non-numbers
+
+# ### Varargs methods
+
+f(args::Number...) = "$(length(args))-ary heterogeneous call"
+f(args::T...) where {T<:Number} = "$(length(args))-ary homogeneous call"
+
+#-
+
+f(1)
+
+#-
+
+f(1, 2, 3)
+
+#-
+
+f(1, 1.5, 2)
+
+#-
+
+f()
+
+#-
+
+f(1, 2) # <== previous 2-arg method is more specific
+
+#-
+
+f("foo") # <== still doesn't apply to non-numbers
+
+#-
+
+## "splat" (more below)
+f([1, 2, 3]...)
+
+# ### Optional Arguments
+
+h(x, y = 0) = 2x + 3y
+
+#-
+
+methods(h)
+
+# Shorthand for this:
 # ```
-# is the one you wrote.
+# h(x, y) = 2x + 3y
+# h(x) = h(x, 0)
+# ```
 
-@assert foo(true) == "foo with one boolean!"
+#-
 
+# ### Keyword Arguments
+
+k(x, y = 0; opt::Bool = false) = opt ? 2x+y : x+2y
+
+#-
+
+k(2)
+
+#-
+
+k(2, 3)
+
+#-
+
+k(2, opt=true)
+
+#-
+
+k(2, 3, opt=true)
+
+#-
+
+foo(x, y; req::Bool) = req ? 2x+y : x+2y
+
+#-
+
+foo(2, 3)
+
+#-
+
+methods(k)
+
+#-
+
+k(2, opt=true)
+
+# ### Keyword arguments: slurp and splat
+
+function allkw(; kw...)
+    @show keys(kw)
+end
+
+#-
+
+allkw(a=1,b=2)
+
+# Just like iterators can be splatted as positional arguments, dict-like collections and named tuples can be splatted as keyword arguments.
+
+function rect(;width=1,height=1,fill="#")
+    for i in 1:height
+        println(fill^width)
+    end
+end
+
+#-
+
+params = (width=8,height=3,fill='A')
+
+#-
+
+rect(; params...)
+
+# ### Exercises
+#
+# #### Exercise 1
+#
+# Write a function that repeats a string an integer number of times which takes the arguments in either order.
+#
+# #### Exercise 2a
+#
+# Write a function `F` that returns the tuple `(x, y, k)` where:
+# - `x` is the first positional argument and is mandatory
+# - `y` is the second positional argument and is optional
+# - `k` is an optional keyword argument
+#
+# The optional arguments should have the following defaults:
+# - `y` defaults to `2x`
+# - `k` defaults to `2y`
+#
+# #### Exercise 2b
+#
+# Write a function `G` just like `F` but with differet defaults:
+# - `k` defaults to `2x`
+# - `y` defaults to `2k`
